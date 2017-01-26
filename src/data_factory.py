e783fe3bf4178a47f02f3b1a051b70d35b55130c
Matthew Compton <matthewc174@gmail.com>
2017-01-25 23:50:37 -0500

    Did some minor messing around.. whoo hoo

--- src/data_factory.py
+++ src/data_factory.py
@@ -1,22 +1,32 @@
 from OAuth2 import reddit
 from twisted.internet import task, reactor
-timeout = 60.0
+#timeout = 60.0
 
-def checkForTimes():
-    print("function will go here")
+#def checkForTimes():
+ #   print("function will go here")
 
-runCheck = task.LoopingCall(checkForTimes())
-runCheck.start(timeout)
+#runCheck = task.LoopingCall(checkForTimes())
+#runCheck.start(timeout)
 
-reactor.run()
+#reactor.run()
 
 
 
 submission_id_list = [] # will collect the submission id's for comparision later.
 
+submission = {
+    ''
+}
 
-for submission in reddit.subreddit('learnpython').hot(limit=10):
+
+for submission in reddit.subreddit('all').hot(limit=10):
     submission_id_list.append(submission.id)
-    print(submission.title)
+
 
 print(submission_id_list)
+
+for ID in submission_id_list:
+    submission = reddit.submission(id=ID)
+    print(submission.title + '\tScore: ' + str(submission.score) + '\n' +submission.url +'\n') #Outputs some of the data grabbed earlier in a formatted way
+
+
